df37f70d9e08be0ae0d5b49ddc818e603a033bb3
Deepikapc <deepu.deepika.pc@gmail.com>
2017-03-31 16:06:16 +0530

    Rspec daily_invoice_controller

--- spec/controllers/daily_invoices_controller_spec.rb
+++ spec/controllers/daily_invoices_controller_spec.rb
@@ -53,15 +53,15 @@ RSpec.describe DailyInvoicesController, type: :controller do
   describe "GET #show" do
     before do
       @user=User.create(name:"navya",email: "navya@gmail.com",cost_of_meal: 65)
-      @daily_invoice=DailyInvoice.create(restaurant_name: "Adigas", amount: "500", date: "2017-3-30",image: "")
+      @daily_invoice=DailyInvoice.create(restaurant_name: "Adigas", amount: "500", date: "2017-3-30",deleted_at: "2")
       @expense = MealsExpense.create(daily_invoice_id: @daily_invoice.id,date: "2017-3-30",had_lunch:"true",user_id: @user.id)
       puts @expense.inspect
   end
       it "should go to show page" do
-        get :show , id: @expense.id
+        get :show , id: @daily_invoice.id
         expect(response.content_type).to eq "text/html"
         expect(response).to render_template(:show)
-        expect(Expense.type).to eq "MealsExpense"
+        expect(@expense.type).to eq "MealsExpense"
       end
   end
 
@@ -70,20 +70,17 @@ RSpec.describe DailyInvoicesController, type: :controller do
     before do
       @daily_invoice=DailyInvoice.create(restaurant_name: "Adigas", amount: "500", date: "2017-3-30")
       @user=User.create(name:"navya",email: "navya@gmail.com",cost_of_meal: 65)
-
+      #@expense = MealsExpense.create(daily_invoice_id: @daily_invoice.id,date: "2017-3-30",had_lunch:"true",user_id: @user.id)
     end
-
     context "When saved succesfully." do
     it "should redirect dailyinvoice_path and expenses_path" do
-      #post :create,daily_invoice: {id: @daily_invoice.id,restaurant_name: "Adigas", amount: "500", date: "2017-3-30",image: ""}
+      #post :create,daily_invoice: {restaurant_name: "Adigas", amount: "500", date: "2017-3-30",image: ""}
       post :create,expense: {daily_invoice_id: @daily_invoice.id, date: "2017-3-30", had_lunch: "true", user_id: @user.id}
-
       expect(response.content_type).to eq "text/html"
       expect(response).to redirect_to daily_invoices_path
-
     end
     end
-
+  end
     # context "POST #DailyInvoice and Expenses." do
     #   it "should render to new page" do
     #     post :create,id: nil
@@ -93,8 +90,40 @@ RSpec.describe DailyInvoicesController, type: :controller do
     #
     #   end
     # end
+    describe '#DELETE destroy' do
+      before do
+        @daily_invoice = DailyInvoice.create(restaurant_name: "Adigas", amount: "500", date: "2017-3-30")
+       @meal_expense= MealsExpense.create(daily_invoice_id: @daily_invoice.id,date: "2017-3-30",had_lunch:"true",user_id: 2)
+      puts @meal_expense.inspect
+      end
+      it 'deletes a dailyinvoice' do
 
-end
+        delete :destroy,id: @daily_invoice.id
+        expect(response.content_type).to eq "text/html"
+        #expect(@daily_invoice.deleted_at).not_to eq nil
+        #expect(DailyInvoice.count).to eq 0
+        expect(response).to redirect_to daily_invoices_path
+      end
+    end
+
+  describe '#POST DailyDetail' do
+    before do
+      @user = User.create(name:"navya",email: "navya@gmail.com",cost_of_meal: 65)
+      @daily_invoice = DailyInvoice.create(restaurant_name: "Adigas", amount: "500", date: "2017-03-14")
+      @expense = Expense.create(daily_invoice_id: @daily_invoice.id,date: "2017-03-14",had_lunch:"true",user_id: @user.id)
+        puts @daily_invoice.inspect
+    end
+    context "when date param is present" do
+      it "create a  valid record" do
+        xhr :post, :daily_details,:controller=>"daily_invoices", params:  {"date"=>"2017-03-14"}
+
+        expect(response.content_type).to eq "text/html"
+        #expect(daily_details).to eq "daily_details"
+        #expect(response).to redirect_to daily_details_path
+        expect(response).to render_template(:daily_details)
+      end
+    end
+  end
 end
 
 
