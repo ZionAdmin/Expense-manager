8b5fdd2540b34b11dcdf2e9566dff1f56aa604d9
sganji <sharath.ganji@gmail.com>
2017-09-26 16:57:23 +0530

    renaming daily_invoices table name to invoices  and  daily_invoice_id to invoice_id in expenses

--- db/migrate/20170926094059_rename_column_in_expenses.rb
+++ db/migrate/20170926094059_rename_column_in_expenses.rb
@@ -1,5 +1,5 @@
 class RenameColumnInExpenses < ActiveRecord::Migration[5.0]
   def change
-  rename_column :expenses, :invoice_id, :daily_invoice_id
+  rename_column :expenses, :daily_invoice_id, :invoice_id
   end
 end
